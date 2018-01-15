90dad3e4e9f3bfcc82c45ecd76a93aa1d0b7e466
AntoineToine <antoine.masson@epitech.eu>
2018-01-15 16:42:34 +0100

    Arduino_Bluetooth_Basic : changed -> contrôle plaque chauffante avec liaison bluetooth

--- Arduino_Bluetooth_Basic/Arduino_Bluetooth_Basic.ino
+++ Arduino_Bluetooth_Basic/Arduino_Bluetooth_Basic.ino
@@ -5,23 +5,53 @@
  *  Download the App : 
  *  This program lets you to control a LED on pin 13 of arduino using a bluetooth module
  */
-char data = 0;                //Variable for storing received data
+
+int sensorvalue = 0;
+float temperature = 0;
+int color = 0;
+int temp_target = 25;
+int data = 0;                //Variable for storing received data
+String receive = "";
+
 void setup() 
 {
   Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
-  pinMode(13, OUTPUT);        //Sets digital pin 13 as output pin
+  pinMode(8, OUTPUT);
+  pinMode(A0, INPUT);
 }
+
 void loop()
 {
-  if(Serial.available() > 0)  // Send data only when you receive data:
+  sensorvalue = analogRead(A0);
+  temperature = sensorvalue * (5.0 / 1024.0);
+  temperature = (temperature - 0.5) * 100;
+  if (temperature < temp_target)
+    digitalWrite(8, HIGH);
+  else
+    digitalWrite(8, LOW);
+
+ while (Serial.available() > 0) {
+  if (color != 'R' || receive.length() == 3)
   {
-    data = Serial.read();      //Read the incoming data and store it into variable data
-    Serial.print(data);        //Print Value inside data in Serial monitor
-    Serial.print("\n");        //New line 
-    if(data == '1')            //Checks whether value of data is equal to 1 
-      digitalWrite(13, HIGH);  //If value is 1 then LED turns ON
-    else if(data == '0')       //Checks whether value of data is equal to 0
-      digitalWrite(13, LOW);   //If value is 0 then LED turns OFF
-  }                            
- 
-}                 
+    color = Serial.read();
+    receive = "";
+
+  }
+  else if (color == 'R') {
+    data = Serial.read();
+    receive = receive + char(data);
+    if (receive.length() == 3)
+    {
+      temp_target = map(receive.toInt(), 0, 256, 20, 60);
+      Serial.println("value:          " + receive);
+        Serial.print(temperature);
+  Serial.print(" ");
+  Serial.print(color);
+  Serial.print(" ");
+  Serial.println(temp_target);
+    }
+  }
+  else
+    Serial.read();
+ }
+}       
