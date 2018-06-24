9073ee742b35aace509a83b40109bd4d6391c773
belugu_m <maxime.beluguet@epitech.eu>
2018-06-24 20:52:21 +0200

    Fix arduino plaque chauffante

--- Arduino_Bluetooth_Basic/Arduino_Bluetooth_Basic.ino
+++ Arduino_Bluetooth_Basic/Arduino_Bluetooth_Basic.ino
@@ -1,37 +1,38 @@
-#include <LiquidCrystal.h>
 
-LiquidCrystal monEcran(12,11,7, 6, 5, 4);
-
-int sensorvalue = 0;
-float temperature = 0;
+int sensorvalue1 = 0;
+int sensorvalue2 = 0;
+float temperature1 = 0;
+float temperature2 = 0;
 int color = 0;
-float temp_target = 10;
+float temp_target = 20;
 char data = 0;                //Variable for storing received data
 String receive = "";
 
 void setup()
 {
-   monEcran.begin(16,2);
-  monEcran.clear();
-  monEcran.print("Actuelle | Cible");
-  monEcran.setCursor(0,1);
   Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
   pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);  
   pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
 }
 
 void loop()
 {
-  sensorvalue = analogRead(A0);
-  temperature = sensorvalue * (5.0 / 1024.0);
-  temperature = (temperature - 0.5) * 100;
-  monEcran.setCursor(0,1);
-  monEcran.print(temperature, BIN);
-  if (temperature < temp_target)
+  sensorvalue1 = analogRead(A0);
+  temperature1 = sensorvalue1 * (5.0 / 1024.0);
+  temperature1 = (temperature1 - 0.5) * 100;
+  if (temperature1 < temp_target)
     digitalWrite(8, HIGH);
   else
-    digitalWrite(8, LOW);
-  //Serial.println(temperature);
+    digitalWrite(8, LOW);  
+  sensorvalue2 = analogRead(A1);
+  temperature2 = sensorvalue2 * 5.0 / 1024.0;
+  temperature2 = temperature2 * 100;
+  if (temperature2 < temp_target)
+    digitalWrite(9, HIGH);
+  else
+    digitalWrite(9, LOW);
   while (Serial.available() > 0)
   {
     data = Serial.read();
@@ -39,8 +40,6 @@ void loop()
     {
       temp_target = receive.toFloat();
       receive = "";
-      monEcran.setCursor(12,1);
-      monEcran.print(temp_target);
     }
     else
     {
